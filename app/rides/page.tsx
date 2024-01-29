fda4a6e84c05b5a4684afbc142ba132fa51529e9
Maciej Mądry <maciejmadry0@gmail.com>
2024-01-29 10:11:38 +0100

    ADD: uuid to key of div wrapper

--- app/rides/page.tsx
+++ app/rides/page.tsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react';
+import { v4 as uuid } from 'uuid';
 
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
 import LoadingSkeleton from '@/app/rides/_components/LoadingSkeleton';
@@ -23,22 +24,24 @@ const RidesPage = async ({
   searchParams: { country, city, street, page = '1', rideType, sort, bikeType },
 }: RidesPageProps) => {
   return (
-    <MaxWidthWrapper className="pt-20 pb-20" key={Math.random()}>
-      <header className="px-2 py-4 md:py-0 mb-12 border shadow-sm shadow-primary rounded">
-        <h1 className="text-center font-semibold text-2xl pt-8 pb-10 underline underline-offset-4 decoration-primary">
-          {`Rides starting ${
-            street ? `at ${street}, ${city}` : `in ${city}`
-          }, ${country}.`}
-        </h1>
-        <Filters />
-      </header>
+    <div key={uuid()}>
+      <MaxWidthWrapper className="pt-20 pb-20" key={Math.random()}>
+        <header className="px-2 py-4 md:py-0 mb-12 border shadow-sm shadow-primary rounded">
+          <h1 className="text-center font-semibold text-2xl pt-8 pb-10 underline underline-offset-4 decoration-primary">
+            {`Rides starting ${
+              street ? `at ${street}, ${city}` : `in ${city}`
+            }, ${country}.`}
+          </h1>
+          <Filters />
+        </header>
 
-      <Suspense fallback={<LoadingSkeleton />}>
-        <RidesList
-          params={{ country, city, street, page, rideType, bikeType, sort }}
-        />
-      </Suspense>
-    </MaxWidthWrapper>
+        <Suspense fallback={<LoadingSkeleton />}>
+          <RidesList
+            params={{ country, city, street, page, rideType, bikeType, sort }}
+          />
+        </Suspense>
+      </MaxWidthWrapper>
+    </div>
   );
 };
 
